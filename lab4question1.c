bb025e8b210f97ccadb34213b4a457f1e444711b
ran-nope <kavin6819@gmail.com>
2026-08-15 00:11:40 +0530

    Implement infix to postfix expression converter

---
+++ lab4question1.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+void push(char x) {
+    stack[++top] = x;
+}
+
+char pop() {
+    return stack[top--];
+}
+
+int precedence(char x) {
+    if (x == '^')
+        return 3;
+    if (x == '*' || x == '/')
+        return 2;
+    if (x == '+' || x == '-')
+        return 1;
+    return 0;
+}
+
+int main() {
+    char infix[MAX], postfix[MAX];
+    int i, j = 0;
+
+    printf("Enter an infix expression: ");
+    scanf("%s", infix);
+
+    for (i = 0; infix[i] != '\0'; i++) {
+        if (isalnum(infix[i])) {
+            postfix[j++] = infix[i];
+        }
+        else if (infix[i] == '(') {
+            push(infix[i]);
+        }
+        else if (infix[i] == ')') {
+            while (top != -1 && stack[top] != '(')
+                postfix[j++] = pop();
+            pop();
+        }
+        else {
+            while (top != -1 && precedence(stack[top]) >= precedence(infix[i]))
+                postfix[j++] = pop();
+            push(infix[i]);
+        }
+    }
+
+    while (top != -1)
+        postfix[j++] = pop();
+
+    postfix[j] = '\0';
+
+    printf("Postfix expression: %s\n", postfix);
+
+    return 0;
+}
