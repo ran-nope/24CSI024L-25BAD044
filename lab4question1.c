7f396c8ad191da8bcabdf966068ea1cc1e96386b
ran-nope <kavin6819@gmail.com>
2026-08-15 00:17:38 +0530

    Refactor stack operations and add expression evaluation
    Refactored stack implementation to support character and integer stacks. Added functions for infix to postfix conversion, postfix evaluation, and parenthesis matching.

--- lab4question1.c
+++ lab4question1.c
@@ -3,15 +3,26 @@
 
 #define MAX 100
 
-char stack[MAX];
-int top = -1;
+char charStack[MAX];
+int charTop = -1;
 
-void push(char x) {
-    stack[++top] = x;
+int intStack[MAX];
+int intTop = -1;
+
+void pushChar(char x) {
+    charStack[++charTop] = x;
 }
 
-char pop() {
-    return stack[top--];
+char popChar() {
+    return charStack[charTop--];
+}
+
+void pushInt(int x) {
+    intStack[++intTop] = x;
+}
+
+int popInt() {
+    return intStack[intTop--];
 }
 
 int precedence(char x) {
@@ -24,11 +35,13 @@ int precedence(char x) {
     return 0;
 }
 
-int main() {
+void infixToPostfix() {
     char infix[MAX], postfix[MAX];
     int i, j = 0;
 
-    printf("Enter an infix expression: ");
+    charTop = -1;
+
+    printf("\nEnter an infix expression: ");
     scanf("%s", infix);
 
     for (i = 0; infix[i] != '\0'; i++) {
@@ -36,26 +49,170 @@ int main() {
             postfix[j++] = infix[i];
         }
         else if (infix[i] == '(') {
-            push(infix[i]);
+            pushChar(infix[i]);
         }
         else if (infix[i] == ')') {
-            while (top != -1 && stack[top] != '(')
-                postfix[j++] = pop();
-            pop();
+            while (charTop != -1 && charStack[charTop] != '(')
+                postfix[j++] = popChar();
+
+            if (charTop != -1)
+                popChar();
         }
         else {
-            while (top != -1 && precedence(stack[top]) >= precedence(infix[i]))
-                postfix[j++] = pop();
-            push(infix[i]);
+            while (charTop != -1 &&
+                   charStack[charTop] != '(' &&
+                   precedence(charStack[charTop]) >= precedence(infix[i])) {
+                postfix[j++] = popChar();
+            }
+
+            pushChar(infix[i]);
         }
     }
 
-    while (top != -1)
-        postfix[j++] = pop();
+    while (charTop != -1)
+        postfix[j++] = popChar();
 
     postfix[j] = '\0';
 
     printf("Postfix expression: %s\n", postfix);
+}
 
-    return 0;
+void evaluatePostfix() {
+    char exp[MAX];
+    int i, a, b, result;
+
+    intTop = -1;
+
+    printf("\nEnter a postfix expression: ");
+    scanf("%s", exp);
+
+    for (i = 0; exp[i] != '\0'; i++) {
+        if (isdigit(exp[i])) {
+            pushInt(exp[i] - '0');
+        }
+        else {
+            if (intTop < 1) {
+                printf("Invalid expression\n");
+                return;
+            }
+
+            b = popInt();
+            a = popInt();
+
+            switch (exp[i]) {
+                case '+':
+                    result = a + b;
+                    break;
+
+                case '-':
+                    result = a - b;
+                    break;
+
+                case '*':
+                    result = a * b;
+                    break;
+
+                case '/':
+                    if (b == 0) {
+                        printf("Division by zero is not allowed\n");
+                        return;
+                    }
+                    result = a / b;
+                    break;
+
+                case '^':
+                    result = 1;
+                    for (int j = 0; j < b; j++)
+                        result *= a;
+                    break;
+
+                default:
+                    printf("Invalid operator\n");
+                    return;
+            }
+
+            pushInt(result);
+        }
+    }
+
+    if (intTop != 0) {
+        printf("Invalid expression\n");
+        return;
+    }
+
+    printf("Result: %d\n", popInt());
+}
+
+void parenthesisMatching() {
+    char exp[MAX];
+    int i, valid = 1;
+
+    charTop = -1;
+
+    printf("\nEnter an expression: ");
+    scanf("%s", exp);
+
+    for (i = 0; exp[i] != '\0'; i++) {
+        if (exp[i] == '(' || exp[i] == '[' || exp[i] == '{') {
+            pushChar(exp[i]);
+        }
+        else if (exp[i] == ')' || exp[i] == ']' || exp[i] == '}') {
+
+            if (charTop == -1) {
+                valid = 0;
+                break;
+            }
+
+            char x = popChar();
+
+            if ((exp[i] == ')' && x != '(') ||
+                (exp[i] == ']' && x != '[') ||
+                (exp[i] == '}' && x != '{')) {
+                valid = 0;
+                break;
+            }
+        }
+    }
+
+    if (charTop != -1)
+        valid = 0;
+
+    if (valid)
+        printf("Parentheses are balanced\n");
+    else
+        printf("Parentheses are not balanced\n");
+}
+
+int main() {
+    int choice;
+
+    while (1) {
+        printf("\n===== STACK APPLICATIONS =====\n");
+        printf("1. Infix to Postfix\n");
+        printf("2. Postfix Expression Evaluation\n");
+        printf("3. Parenthesis Matching\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                infixToPostfix();
+                break;
+
+            case 2:
+                evaluatePostfix();
+                break;
+
+            case 3:
+                parenthesisMatching();
+                break;
+
+            case 4:
+                return 0;
+
+            default:
+                printf("Invalid choice\n");
+        }
+    }
 }
